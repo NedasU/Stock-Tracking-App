dcdaaa44d14fc3b329daf8a1016b619adb498823
NedasU <nedasurba@gmail.com>
2024-11-11 17:00:37 +0200

    Changing the styling of the signup page

--- frontend/src/Components/SignUp.js
+++ frontend/src/Components/SignUp.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitInfo = async ()=> {
+        if (username.trim() && password.trim()){
+            await fetch
+        }
+    }
     return (
         <div className='signup-container'>
             <div className='sign-in-up-card'>
                 <h2>Getting Started</h2>
                 <input type='text'
                     placeholder='Username'
+                    value={username}
+                    maxLength={255}
+                    onChange={(e) => setUsername(e.target.value)}
                     >
                 </input>
                 <input type='text'
-                    placeholder='Password'>
+                    placeholder='Password'
+                    value={password}
+                    maxLength={255}
+                    onChange={(e) => setPassword(e.target.value)}>
                 </input>
                 
                 <div className='submit-switch-container'>
