1b397485b9e632849d397a8e065f681ef7bc3254
NedasU <nedasurba@gmail.com>
2024-11-11 15:31:26 +0200

    SCRUM-20: Create Users Model, added error checking through try and catch blocks

---
+++ backend/src/models/User.js
@@ -0,0 +1,64 @@
+const db = require('../db/db')
+
+class User{
+
+    constructor(id, username, password_hash){
+        this.id = id;
+        this.username = username;
+        this.password_hash = password_hash;
+    }
+
+    static async findbyid(id){
+        try{
+            const [rows] = await db.execute("SELECT * FROM Users WHERE id = ?", [id]);
+            if (rows.length > 0){
+                const {id, username, password_hash} = rows[0];
+                return new User(id, username, password_hash);
+            }
+            return null;
+        } catch (error) {
+            console.error("An error occured: ", error)
+            throw error
+        }
+    }
+
+    static async findbyUsername(username){
+        try {
+            const [rows] = await db.execute("SELECT * FROM Users WHERE username = ?", [username]);
+            if (rows.length > 0){
+                const {id, username, password_hash} = rows[0];
+                return new User(id, username, password_hash);
+            }
+            return null;
+        } catch (error){
+            console.error("An Error occured: ", error)
+            throw error;
+        }
+    }
+
+    static async addNewUser(username, password_hash){
+        try {
+            const [result] = await db.execute("INSERT INTO Users (username, password_hash) VALUES (? ?)", [username, password_hash])
+            const user_id = result.insertId;
+            const found = await db.execute("SELECT * FROM Users WHERE id = ?", [user_id])
+            if (found.length > 0){
+                const {id, username, password_hash} = found[0];
+                return new User(id, username, password_hash);
+            }
+            return null;
+        } catch (error){
+            console.error("An Error occured: ", error)
+            throw error;
+        }
+    }
+
+    static async removeUserById(id){
+        try{
+            const result = await db.execute("DELETE FROM Users WHERE id = ?", [id]);
+            return result;
+        } catch (error){
+            console.error("An Error occured: ", error)
+            throw error;
+        }
+    }
+}
